54970ddd685f3b1fc0f967725a2d42471ff7f264
Víctor Faria <vitilevi@hotmail.com>
2021-07-29 02:11:22 -0300

    deploy

---
+++ precache-manifest.7640857493a422ae0af22b1651e5bee0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613d9048fb3d43d42b5b",
+    "url": "/cnpj-check/css/app.5cef277a.css"
+  },
+  {
+    "revision": "9b553b2550cb21a492fc7b817a06d304",
+    "url": "/cnpj-check/index.html"
+  },
+  {
+    "revision": "613d9048fb3d43d42b5b",
+    "url": "/cnpj-check/js/app.9d6cc5da.js"
+  },
+  {
+    "revision": "cf8d2edc232b25f2e3d8",
+    "url": "/cnpj-check/js/chunk-vendors.81077fac.js"
+  },
+  {
+    "revision": "6b5daac96d9cce7582525cd618c28b88",
+    "url": "/cnpj-check/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cnpj-check/robots.txt"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/cnpj-check/vue.ico"
+  }
+]);
